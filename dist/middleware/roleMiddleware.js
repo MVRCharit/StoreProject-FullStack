88c26c387751f96bc88131c163bb889e66f9384b
24f2000903 <24f2000903@ds.study.iitm.ac.in>
2026-09-15 21:50:34 +0530

    Untrack ignored files and clear cache

--- dist/middleware/roleMiddleware.js
+++
@@ -1,7 +0,0 @@
-export const isAdmin = (req, res, next) => {
-    const user = req.user;
-    if (user.role !== "admin") {
-        return res.status(403).json({ message: "Admin access required" });
-    }
-    next();
-};
--- dist/rabbitmq/producer.js
+++
@@ -1,7 +0,0 @@
-import amqp from "amqplib";
-export const sendToQueue = async (queue, data) => {
-    const conn = await amqp.connect("amqp://localhost");
-    const channel = await conn.createChannel();
-    await channel.assertQueue(queue);
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
-};
