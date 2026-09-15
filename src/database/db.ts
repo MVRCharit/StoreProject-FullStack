efb75033ff0fddd424d3e8c1dc0cf2585208156e
24f2000903 <24f2000903@ds.study.iitm.ac.in>
2026-09-15 22:09:36 +0530

    Untrack ignored files and clear cache

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,13 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "charit",
-  database: "testdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export async function initDB() {
